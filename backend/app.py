d08db16cb8b22c561f3a99228a62671d544b86fa
kabindra <kabindramagar32@gmail.com>
2026-02-07 17:30:12 +0545

    integrated fronted dashboard with flask api (user, vendor, order)

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,24 @@
-import os
 from flask import Flask, jsonify
+from flask_cors import CORS
 from extensions import db
 
 from routes.user_routes import user_bp
 from routes.vendor_routes import vendor_bp
 from routes.order_routes import order_bp
 
+import os
+
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    DB_PATH = os.path.join(BASE_DIR, "instance", "data.db")
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + DB_PATH
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASE_DIR, "instance", "data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    os.makedirs(os.path.join(BASE_DIR, "instance"), exist_ok=True)
 
     db.init_app(app)
 
@@ -32,7 +36,6 @@ def create_app():
     return app
 
 
-app = create_app()
-
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
